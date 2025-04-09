98ce0a717c285700193937b5aafaa07b099c5521
aqsaaQaazi <aqsaakqaazi@gmail.com>
2025-04-09 06:09:18 +0500

    03 down

---
+++ 01_expressions/03_In_into_feet.py
@@ -0,0 +1,11 @@
+# inches in foot = 12
+
+def main():
+    feet = float(input("Enter feet you want to convert: "));
+    inches = feet * 12
+    print(f"that would be {inches:.2f} inches in {feet} feet. ")
+    
+if __name__ == "__main__":
+    main(
+        
+    )
